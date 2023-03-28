85c94a704bdad9dcbe92c4bc686dd2a96a205a6f
Angel Modesto <amodesto@myrobal.com>
2023-03-27 19:30:53 -0600

    Fixed combo countries,states,cities

--- Shopping/Shopping/Controllers/AccountController.cs
+++ Shopping/Shopping/Controllers/AccountController.cs
@@ -8,111 +8,115 @@ using Shopping.Models;
 
 namespace Shopping.Controllers
 {
-	public class AccountController : Controller
-	{
-		private readonly IUserHelper _userHelper;
-		private readonly DataContext _context;
-		private readonly ICombosHelper _combosHelper;
-		private readonly IBlobHelper _blobHelper;
+    public class AccountController : Controller
+    {
+        private readonly IUserHelper _userHelper;
+        private readonly DataContext _context;
+        private readonly ICombosHelper _combosHelper;
+        private readonly IBlobHelper _blobHelper;
 
-		public AccountController(IUserHelper userHelper, DataContext context, ICombosHelper combosHelper, IBlobHelper blobHelper)
-		{
-			_userHelper = userHelper;
-			_context = context;
-			_combosHelper = combosHelper;
-			_blobHelper = blobHelper;
-		}
+        public AccountController(IUserHelper userHelper, DataContext context, ICombosHelper combosHelper, IBlobHelper blobHelper)
+        {
+            _userHelper = userHelper;
+            _context = context;
+            _combosHelper = combosHelper;
+            _blobHelper = blobHelper;
+        }
 
-		public IActionResult Login()
-		{
-			if (User.Identity.IsAuthenticated)
-			{
-				return RedirectToAction("Index", "Home");
-			}
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
 
-			return View(new LoginViewModel());
-		}
+            return View(new LoginViewModel());
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> Login(LoginViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				Microsoft.AspNetCore.Identity.SignInResult result = await _userHelper.LoginAsync(model);
-				if (result.Succeeded)
-				{
-					return RedirectToAction("Index", "Home");
-				}
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Microsoft.AspNetCore.Identity.SignInResult result = await _userHelper.LoginAsync(model);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
 
-				ModelState.AddModelError(string.Empty, "Email o contraseña incorrectos.");
-			}
+                ModelState.AddModelError(string.Empty, "Email o contraseña incorrectos.");
+            }
 
-			return View(model);
-		}
+            return View(model);
+        }
 
-		public async Task<IActionResult> Logout()
-		{
-			await _userHelper.LogoutAsync();
-			return RedirectToAction("Index", "Home");
-		}
+        public async Task<IActionResult> Logout()
+        {
+            await _userHelper.LogoutAsync();
+            return RedirectToAction("Index", "Home");
+        }
 
-		public IActionResult NotAuthorized()
-		{
-			return View();
-		}
-		public async Task<IActionResult> Register()
-		{
-			AddUserViewModel model = new AddUserViewModel
-			{
-				Id = Guid.Empty.ToString(),
-				Countries = await _combosHelper.GetComboCountriesAsync(),
-				States = await _combosHelper.GetComboStatesAsync(0),
-				Cities = await _combosHelper.GetComboCitiesAsync(0),
-				UserType = UserType.User,
-			};
+        public IActionResult NotAuthorized()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Register()
+        {
+            AddUserViewModel model = new AddUserViewModel
+            {
+                Id = Guid.Empty.ToString(),
+                Countries = await _combosHelper.GetComboCountriesAsync(),
+                States = await _combosHelper.GetComboStatesAsync(0),
+                Cities = await _combosHelper.GetComboCitiesAsync(0),
+                UserType = UserType.User,
+            };
 
-			return View(model);
-		}
+            return View(model);
+        }
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Register(AddUserViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				Guid imageId = Guid.Empty;
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(AddUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = Guid.Empty;
 
-				if (model.ImageFile != null)
-				{
-					imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "users");
-				}
+                if (model.ImageFile != null)
+                {
+                    imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "users");
+                }
 
-				model.ImageId = imageId;
+                model.ImageId = imageId;
 
-				User user = await _userHelper.AddUserAsync(model);
-				if (user == null)
-				{
-					ModelState.AddModelError(string.Empty, "Este correo ya está siendo usado.");
+                User user = await _userHelper.AddUserAsync(model);
+                if (user == null)
+                {
+
+                    ModelState.AddModelError(string.Empty, "Este correo ya está siendo usado.");
+                    model.Countries = await _combosHelper.GetComboCountriesAsync();
+                    model.States = await _combosHelper.GetComboStatesAsync(model.CountryId);
+                    model.Cities = await _combosHelper.GetComboCitiesAsync(model.StateId);
 					return View(model);
-				}
+                }
 
-				LoginViewModel loginViewModel = new LoginViewModel
-				{
-					Password = model.Password,
-					RememberMe = false,
-					Username = model.Username
-				};
+                LoginViewModel loginViewModel = new LoginViewModel
+                {
+                    Password = model.Password,
+                    RememberMe = false,
+                    Username = model.Username
+                };
 
-				var result2 = await _userHelper.LoginAsync(loginViewModel);
+                var result2 = await _userHelper.LoginAsync(loginViewModel);
 
-				if (result2.Succeeded)
-				{
-					return RedirectToAction("Index", "Home");
-				}
-			}
+                if (result2.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
 
-			return View(model);
-		}
+            return View(model);
+        }
 
         public JsonResult GetStates(int countryId)
         {
