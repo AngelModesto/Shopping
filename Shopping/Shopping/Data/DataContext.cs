b13ef2e0d23af738a57a252d1d8ad282e1598f38
Angel Modesto <amodesto@myrobal.com>
2023-03-28 18:53:38 -0600

    Add products

--- Shopping/Shopping/Data/DataContext.cs
+++ Shopping/Shopping/Data/DataContext.cs
@@ -15,6 +15,13 @@ namespace Shopping.Data
         public DbSet<Country> Countries { get; set; }
         public DbSet<State> States { get; set; }
 
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+
+
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -22,6 +29,9 @@ namespace Shopping.Data
             modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
             modelBuilder.Entity<State>().HasIndex("Name", "CountryId").IsUnique();
             modelBuilder.Entity<City>().HasIndex("Name", "StateId").IsUnique();
+            modelBuilder.Entity<Product>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<ProductCategory>().HasIndex("ProductId", "CategoryId").IsUnique();
+
         }
     }
 }
--- Shopping/Shopping/Data/Entities/Category.cs
+++ Shopping/Shopping/Data/Entities/Category.cs
@@ -11,5 +11,8 @@ namespace Shopping.Data.Entities
         [MaxLength(50, ErrorMessage = "El campo {0} no debe tener máximo {1} caractéres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Name { get; set; }
+
+        public ICollection<ProductCategory> ProductCategories { get; set; }
+
     }
 }
