67e5117cc0b0644909c4d805c20943d53634e29b
Angel Modesto <amodesto@myrobal.com>
2023-03-30 18:00:29 -0600

    Order, filter, pagination in Home and add privacy policy

--- Shopping/Shopping/Models/HomeViewModel.cs
+++ Shopping/Shopping/Models/HomeViewModel.cs
@@ -1,10 +1,14 @@
-﻿using Shopping.Data.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Common;
+using Shopping.Data.Entities;
 
 namespace Shopping.Models
 {
     public class HomeViewModel
     {
-        public ICollection<Product> Products { get; set; }
+        public PaginatedList<Product> Products { get; set; }
+
+        public ICollection<Category> Categories { get; set; } 
 
         public float Quantity { get; set; }
     }
